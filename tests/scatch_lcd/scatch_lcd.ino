4fab3b37355610a82791804c93d93555aa04e4e8
ogneyar <ogneyar-ne@ya.ru>
2021-01-02 20:23:18 +0300

    ttt

--- tests/scatch_lcd/scatch_lcd.ino
+++ tests/scatch_lcd/scatch_lcd.ino
@@ -3,59 +3,119 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем какой дисплей
 
-boolean flag = true;
-int i = 0;
-boolean pic = true;
+boolean flag;
+int i, j, k, m;
 const char* pict;
-boolean oO = true;
+char * first_string, * fi_string;
+word last_time;
 
 void setup()
 {
-//  pinMode(5,OUTPUT);  
-    pinMode(13,OUTPUT);
 
+  Serial.begin(9600);
+  Serial.println("Hell o world");
+
+
+  pinMode(13,OUTPUT);
   // инициализируем дисплей
   lcd.init();
-
   // Включаем подсветку дисплея
   lcd.backlight();
-
   // переходим на позицию - первый ноль это позиция символа
   // второй ноль это позиция строки
-  lcd.setCursor(0, 0);
-  
+  lcd.setCursor(0, 0);  
   // выводим надпись 
-  lcd.print("-=Hell o World=-");
-  
-  // переходим на позицию - первый ноль это позиция символа
-  // вторая единица это позиция строки
-//  lcd.setCursor(7, 1);
+  first_string = "-=Hell o World=-";  
+  lcd.print(first_string);
+  fi_string = first_string;
 
-  // выводим надпись
-//  lcd.print("oO");
+  delay(1000);
+
+  i = 0;
+  j = 15;
+  k = 0;
+  pict = " ";
+  flag = true;
+  last_time = millis();
 }
+
+
 void loop()
-{
-//  if (pic) pict = "oO";
-//  else pict = "Oo";
-
-  if (oO) pict = "o";
-  else pict = "O";
-
-//  digitalWrite(5, flag = !flag);
-  digitalWrite(13, flag = !flag);
-
-  delay(100);
+{  
   
-  // переходим на позицию
-  lcd.setCursor(i, 1);
-  // выводим надпись
-  lcd.print(pict);
-  
-  i++;
-  if (i > 15){
-    i = 0;
-    oO = !oO;
+  if (millis() - last_time > 200) {
+    
+    last_time = millis();
+    digitalWrite(13, flag = !flag);
+
+
+    // --------------------
+    // рисуем первую строку
+    // --------------------
+
+   if ( j == 15 ) {
+      lcd.setCursor(0, 0);  
+      lcd.print("                ");
+   }
+
+    // переходим на позицию
+    lcd.setCursor(j, 0);
+    // выводим надпись
+    lcd.print(fi_string);
+
+    Serial.println(fi_string);
+
+    if (k > 15){
+      k = 0;
+      j--;
+      fi_string = first_string;
+    } 
+
+    if (j == 0) {
+      k++;
+      m = 0;
+      fi_string = "";
+      for (int r = k; r < 16; r++) {
+        fi_string[m] = first_string[r];
+        // lcd.setCursor(m, 0);
+        // lcd.print(first_string[r]);
+        m++;
+      }
+      for (int s = m; s < 16; s++) {
+        fi_string[s] = ' ';
+        // lcd.setCursor(m, 0);
+        // lcd.print(" ");
+      }
+      // lcd.setCursor(0, 0);
+      // lcd.print(fi_string);
+      // delay(50);
+    }else j--;
+    
+
+    if ( j < 0 ) j = 15;
+
+
+    // --------------------
+    // рисуем вторую строку
+    // --------------------
+
+    // переходим на позицию
+    lcd.setCursor(i, 1);
+    // выводим надпись
+    lcd.print(pict);
+    if ( i != 0 || pict != " " ) {
+      i++;
+    }
+    if ( i == 0 ) {
+      pict = "O ";
+    }
+    if ( i > 15 ){
+      i = 0;
+      pict = " ";
+    }
+
+    delay(30);
+
   }
-  oO = !oO;
+
 }
