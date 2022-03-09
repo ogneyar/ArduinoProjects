6f85797ed7f61948c959864e94b9bd52ef854cfe
ogneyar <ogneyar-ne@ya.ru>
2022-03-09 20:28:58 +0300

    eee

--- Scatches/ledRun_7/ledRun_7.ino
+++ Scatches/ledRun_7/ledRun_7.ino
@@ -2,28 +2,42 @@
 boolean flagButton;
 boolean button;
 boolean flagLed;
-unsigned long last_press;
-unsigned long PORT_BUTTON = 3;
-unsigned long PORT_LED = 5;
+short last_press;
+short BUTTON = 3;
+short LED = 5;
 
 void setup() {
-  pinMode(PORT_BUTTON, INPUT_PULLUP);
-  pinMode(PORT_LED, OUTPUT);
+//  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(BUTTON, INPUT);
+//  digitalWrite(BUTTON, 0);
+  pinMode(LED, OUTPUT);
+//  digitalWrite(LED, 0);
   Serial.begin(9600); 
 }
 
 void loop() {
-    button = !digitalRead(PORT_BUTTON);
-    if (button == true && flagButton == false && millis() - last_press > 50) {
-      flagButton = true;
-      Serial.println("Button pressed");
-      flagLed =!flagLed;
-      digitalWrite(PORT_LED, flagLed);
+    button = digitalRead(BUTTON);
+    delay(3);
+    //if (button == true && flagButton == false && millis() - last_press > 50) {
+    //  flagButton = true;
+    //  Serial.println("Button pressed");
+    //  flagLed =!flagLed;
+    //  digitalWrite(PORT_LED, flagLed);
 //      digitalWrite(13, flagLed);
-      last_press = millis();
+    //  last_press = millis();
+    //}
+    //if (button == false && flagButton == true) {
+    //  flagButton = false;
+    //  Serial.println("Button released");
+    //}
+    if (button == true) {
+      digitalWrite(LED, 0);
+//      delay(30);
+    }else if (button == false) {
+      digitalWrite(LED, 0);
+      delay(30);
+      digitalWrite(LED, 1);
+      delay(30);
     }
-    if (button == false && flagButton == true) {
-      flagButton = false;
-      Serial.println("Button released");
-    }
+    //digitalWrite(LED, 0);
 }
