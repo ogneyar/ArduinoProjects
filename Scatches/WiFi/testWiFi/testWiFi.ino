576c414064e3d86e02a8c0a559fe76728873bb3f
ogneyar <ogneyar-ne@ya.ru>
2022-06-06 16:50:46 +0300

    eee

--- Scatches/WiFi/testWiFi/testWiFi.ino
+++ Scatches/WiFi/testWiFi/testWiFi.ino
@@ -1,16 +1,19 @@
 
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <WiFiClient.h>
 
 #define HOST "api.telegram.org"
-//#define HOST "localhost:5000"
-#define SSL_PORT 443
-#define PORT 80
+#define PORT 443
+WiFiClientSecure client;
 
-//#define BOTtoken "5328519984:AAGa4HzzxvoOZF2cGdW3G6VR0eR07ieRrW0"
-String token = "5328519984:AAGa4HzzxvoOZF2cGdW3G6VR0eR07ieRrW0";
-String command = "bot" + token;
+//#define HOST "192.168.0.244"
+//#define PORT 5000
+//WiFiClient client;
+
+#define TOKEN "5328519984:AAGa4HzzxvoOZF2cGdW3G6VR0eR07ieRrW0"
+
+String command = "";
 String mess = "";
 
 char ssid[] = "MyWiFi)";    //  your network SSID (name)
@@ -18,13 +21,12 @@ char pass[] = "11111111";   // your network password
 
 bool headerEnd = false;
 bool avail = false;
-long now = millis();
+long now;// = millis();
 int ch_count = 0;
 int longPoll = 0;
 int waitForResponse = 1500;
 int maxMessageLength = 1300;
 
-WiFiClientSecure client;
 
 
 void setup() {
@@ -46,15 +48,22 @@ void setup() {
     printWifiStatus();
 
     delay(10);
-    client.setInsecure();
+    if (HOST == "api.telegram.org") client.setInsecure();
 
     Serial.println("\nStarting connection to server...");
+    Serial.print("HOST: ");
+    Serial.println(HOST);
+    Serial.print("PORT: ");
+    Serial.println(PORT);
+    Serial.println();
 
-    if (client.connect(HOST, SSL_PORT)) {
+    if (client.connect(HOST, PORT)) {
 
-        Serial.println("connected to server");
-
-        String command = "bot" + token + "/getMe";
+        Serial.println("Connected to server!");
+        Serial.println();
+        
+        if (HOST == "api.telegram.org") command = "bot" + (String)TOKEN + "/getMe";
+        else command = "api/test";
         
         // Make a HTTP request:
         client.print("GET /");
@@ -63,11 +72,13 @@ void setup() {
         client.print("Host: ");
         client.println(HOST);
         client.println("Accept: application/json");
+        client.println("User-Agent: ESP2866");
         client.println("Cache-Control: no-cache");
         client.println();
 
         Serial.println(command);
         
+        now = millis();
 
         while (millis() - now < longPoll * 1000 + waitForResponse) {
 
@@ -89,36 +100,49 @@ void setup() {
             if (avail) {
                 Serial.println();
                 Serial.println(mess);
-                Serial.println();
 
                 DynamicJsonBuffer jsonBuffer;
                 JsonObject &root = jsonBuffer.parseObject(mess);
 
-                closeClient();
-
+                //closeClient();
             
                 if (root.success()) {
-                    if (root.containsKey("result")) {
-                    unsigned int id = root["result"]["id"];
-                    String name = root["result"]["first_name"];
-                    String username = root["result"]["username"];
-                    
-                        Serial.print("\nid = ");
+                    if (root.containsKey("result") && root["result"]["id"]) {
+                        unsigned int id = root["result"]["id"];
+                        String name = root["result"]["first_name"];
+                        String username = root["result"]["username"];
+                        
+                        Serial.println();
+                        Serial.print("id = ");
                         Serial.println(id);
-                        Serial.print("\nname = ");
+                        Serial.print("name = ");
                         Serial.println(name);
-                        Serial.print("\nusername = ");
+                        Serial.print("username = ");
                         Serial.println(username);
                         Serial.println();
+                    }else {
+                        // JsonObject result = root["result"];
+                        String article = root["result"]["article"];
+                        String url = root["result"]["url"];
+                        unsigned int price = root["result"]["price"];
                         
+                        Serial.println();
+                        Serial.print("article = ");
+                        Serial.println(article);
+                        Serial.print("url = ");
+                        Serial.println(url);
+                        Serial.print("price = ");
+                        Serial.println(price);
+                        Serial.println();
                     }
-                    
                 }
-                
                 break;
             }
         }
 
+    }else {
+        Serial.println();
+        Serial.println("NO connecting from server.");
     }
 
     // if the server's disconnected, stop the client:
