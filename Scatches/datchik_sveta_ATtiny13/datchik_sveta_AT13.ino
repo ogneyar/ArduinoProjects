cd55e9bf5029770690c5a8de1ac78341c55cbf0a
ogneyar <ogneyar-ne@ya.ru>
2022-06-11 09:17:14 +0300

    edit

--- Scatches/datchik_sveta_ATtiny13/datchik_sveta_AT13.ino
+++ Scatches/datchik_sveta_ATtiny13/datchik_sveta_AT13.ino
@@ -1,5 +1,5 @@
-//int sensorPin = 0;//A0;//
-int ledPin = 1;//D2;//
+//int sensorPin = 0;
+int ledPin = 1;
 //unsigned int sensor = 0;
 byte del = 50;
 
@@ -8,14 +8,12 @@ void setup() {
 //  pinMode(sensorPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-//  Serial.begin(9600);
 }
 
 void loop() {
 //  sensor = analogRead(sensorPin);
 //  if (sensor < 350) digitalWrite(ledPin, HIGH);
 //  else digitalWrite(ledPin, LOW);
-//  Serial.println(sensor, DEC);
 //  delay(del);
   
   digitalWrite(ledPin, HIGH);
