fe68fd2b1aa5f40150225ef7d63e35507e4a8d8b
ogneyar <ogneyar-ne@ya.ru>
2022-06-25 16:55:07 +0300

    tests

--- Scatches/SIM800/test/test.ino
+++ Scatches/SIM800/test/test.ino
@@ -1,17 +1,20 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial SIM800(6,3); // rx sim in 6 pin // tx sim in 3 pin
+SoftwareSerial SIM800(6,7); // rx sim in 6 pin // tx sim in 7 pin
+
+uint32_t bod = 115200; // 9600; // 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(bod);
   Serial.println("Start");
-  SIM800.begin(115200);
+  SIM800.begin(bod);
   SIM800.println("AT");
 }
 
 void loop() {
-  if (SIM800.available())
-    Serial.write(SIM800.read());
-   if (Serial.available())
-    SIM800.write(Serial.read());
+  if (SIM800.available()) Serial.write(SIM800.read());
+  
+  if (Serial.available()) SIM800.write(Serial.read());
+  
+  _delay_ms(100);
 }
