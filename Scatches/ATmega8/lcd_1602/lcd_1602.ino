178c0a17498514fb1a810803c8ccd90b870ea0a8
ogneyar <ogneyar-ne@ya.ru>
2022-06-26 14:49:29 +0300

    eee

--- Scatches/ATmega8/lcd_1602/lcd_1602.ino
+++ Scatches/ATmega8/lcd_1602/lcd_1602.ino
@@ -10,18 +10,7 @@ const char first_string[17] = "-=Hell o World=-";
 word last_time;
 
 void setup()
-{  
-  i = 0; // 8 байт (если 0)  
-  j = 15; // 12 байт (если > 0)
-  k = 0;  
-  pict = (char *)" "; // 12 байт (если " ") + 2 байта за каждые 2 английских символа или 1 русских
-  flag = true; // 4 байта если false и 6 байт если true  
-  last_time = millis(); // 8 байт (если 0), 12 байт (если 1) и 30 байт (если millis()) 
-  fi_string = (char *)first_string; // 12 байт
-
-//  Serial.begin(9600); // 6 байт
-//  Serial.println("Hell o world"); // 38 байт
-
+{
   pinMode(13, OUTPUT);
   // инициализируем дисплей
   lcd.init();
@@ -40,7 +29,5 @@ void setup()
 void loop()
 {
 
-
-  }
 
 }
