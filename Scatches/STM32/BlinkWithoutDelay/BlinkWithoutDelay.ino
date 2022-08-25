63518a24ec91e1529da06a7afa6fbb09650c9ec0
ogneyar <ogneyar-ne@ya.ru>
2022-08-25 12:45:36 +0300

    eee

---
+++ Scatches/STM32/BlinkWithoutDelay/BlinkWithoutDelay.ino
@@ -0,0 +1,17 @@
+byte ledPin =  LED_BUILTIN;
+bool ledState = LOW;
+unsigned long previousMillis = 0;
+unsigned long interval = 1000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
