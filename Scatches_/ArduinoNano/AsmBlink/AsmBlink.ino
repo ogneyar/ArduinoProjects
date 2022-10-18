24fd63a17dc80f8b6a59d4522c9094c6bef40f07
ogneyar <ogneyar-ne@ya.ru>
2022-10-18 16:18:48 +0300

    test asm

---
+++ Scatches_/ArduinoNano/AsmBlink/AsmBlink.ino
@@ -0,0 +1,14 @@
+extern "C" {
+  extern __FlashStringHelper *D13OnOff(byte);
+}
+
+void setup() {
+
+}
+
+void loop() {
+  D13OnOff(0b00000000);
+  delay(450);
+  D13OnOff(0b00100000);
+  delay(50);
+}
