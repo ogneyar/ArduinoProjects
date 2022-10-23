71637f690a3820f0ffa37cfcd8fd521964d0c716
ogneyar <ogneyar-ne@ya.ru>
2022-10-23 10:31:38 +0300

    eeee

--- Keil/newKeilProject/Objects/main.d
+++ Keil/newKeilProject/Objects/main.d
@@ -1,8 +1,8 @@
 ./objects/main.o: main.c MDR32F9x.h \
-  C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\Admin\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  G:\Program\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  G:\Program\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  G:\Program\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  G:\Program\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  G:\Program\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  G:\Program\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
   system_MDR32F9x.h
--- Keil/newKeilProject/main.c
+++ Keil/newKeilProject/main.c
@@ -1,40 +1,47 @@
+
 #include "MDR32F9x.h"
 
-//#include "MDR32Fx.h"
+#define PB1 1
+#define PB2 2
+#define PC2 2
+#define LED_HL1 (1<<PB1) // red
+#define LED_HL2 (1<<PB2) // green
+#define LED_HL3 (1<<PC2) // blue
+#define PORTB_CLK (1<<22)
+#define PORTC_CLK (1<<23)
+#define PORTD_CLK (1<<24)
 
-
-#define LED_VD5 (1<<0) 
-#define PORTB_CLK (1UL<<22)
-
-static uint16_t i;
+//static uint16_t i;
 
 void GPIO_Init(void);
 
 void GPIO_Init(void){
-	MDR_RST_CLK->PER_CLOCK = PORTB_CLK;
+	MDR_RST_CLK->PER_CLOCK |= PORTB_CLK | PORTC_CLK;
 	
-	MDR_PORTB->OE = LED_VD5; 
-	MDR_PORTB->FUNC = 0x00000000; 
-	MDR_PORTB->ANALOG = 0x0000FFFF; 
-	MDR_PORTB->PULL = (LED_VD5) << 16; 
-	MDR_PORTB->PD = ~(uint32_t)(LED_VD5); 
-	MDR_PORTB->PWR = 0x55555555; 
-	MDR_PORTB->GFEN = 0; 
+	MDR_PORTB->OE |= LED_HL1 | LED_HL2; 
+	MDR_PORTB->ANALOG |= LED_HL1 | LED_HL2;
+	MDR_PORTB->PWR |= (0x1<<(PB1*2)) | (0x1<<(PB2*2)); // 01 - lower
+	
+	MDR_PORTC->OE |= LED_HL3; 
+	MDR_PORTC->ANALOG |= LED_HL3;
+	MDR_PORTC->PWR |= (0x1<<(PC2*2)); // 01 - lower
+	
+	MDR_PORTB->RXTX |= LED_HL1;
+	MDR_PORTB->RXTX |= LED_HL2;	
+	MDR_PORTC->RXTX |= LED_HL3;
 }	
 
 int main (void)
 {
 
 	GPIO_Init();
-	
+		
 	while(1) 
 	{
-		MDR_PORTB->RXTX &= ~(unsigned int)LED_VD5; 
+		/*MDR_PORTB->RXTX &= ~(unsigned int)LED_VD5; 
 		for(i=0; i<10000; i++) __asm("nop");
 		MDR_PORTB->RXTX |= (unsigned int)LED_VD5; 
-		for(i=0; i<10000; i++) __asm("nop");
+		for(i=0; i<10000; i++) __asm("nop");*/
 	}
-	
-	//return 0;
 	
 }
