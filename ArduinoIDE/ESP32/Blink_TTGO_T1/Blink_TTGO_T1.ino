7065306285cf21d6703a8262eb560c75c4402c38
ogneyar <ogneyar-ne@ya.ru>
2022-11-23 16:06:36 +0300

    eee

---
+++ ArduinoIDE/ESP32/Blink_TTGO_T1/Blink_TTGO_T1.ino
@@ -0,0 +1,13 @@
+
+// TTGO T1
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
