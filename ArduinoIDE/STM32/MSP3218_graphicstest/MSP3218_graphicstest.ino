fbb6ccb0dc476321657ff9208dda79a60c307739
ogneyar <ogneyar-ne@ya.ru>
2022-12-02 11:20:16 +0300

    tests

--- ArduinoIDE/STM32/MSP3218_graphicstest/MSP3218_graphicstest.ino
+++ ArduinoIDE/STM32/MSP3218_graphicstest/MSP3218_graphicstest.ino
@@ -1,17 +1,22 @@
 
+// STM32F103C8T6
 
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 
 // For the Adafruit shield, these are the default.
-#define TFT_DC 9
-#define TFT_CS 10
+#define TFT_CS PB9
+#define TFT_DC PB8
+#define TFT_MOSI PB7
+#define TFT_CLK PB6
+#define TFT_RST PB5
+#define TFT_MISO PB4
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void setup() {
   Serial.begin(9600);
