7a988d4d8fb91abbb960ef2fdb1d3d1428f88855
ogneyar <ya13th@mail.ru>
2023-03-27 21:40:05 +0300

    eee

--- ArduinoIDE/ArduinoZero/Blink/Blink.ino
+++ ArduinoIDE/ArduinoZero/Blink/Blink.ino
@@ -11,5 +11,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(900);
+  delay(100);
 }
---
+++ Keil/SAMD21/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Test' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM0plus.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
