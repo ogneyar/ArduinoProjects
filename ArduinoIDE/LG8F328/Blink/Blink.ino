f04daa17a78e2b26c7ec5746ee7b6fd380be9086
ogneyar <ya13th@mail.ru>
2023-04-12 10:25:13 +0300

    eee

---
+++ ArduinoIDE/LG8F328/Blink/Blink.ino
@@ -0,0 +1,15 @@
+
+// LGT8F328
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(900);
+}
--- Keil/SWM181/Blink/main.c
+++ Keil/SWM181/Blink/main.c
@@ -7,19 +7,25 @@ int main(void)
 {	
 	SystemInit();
 	
-	GPIO_Init(GPIOA, PIN4, 0, 1, 0, 0);			//输入，上拉使能，接KEY
+	GPIO_Init(GPIOB, PIN0, 0, 1, 0, 0);			//输入，上拉使能，接KEY
 	
-	GPIO_Init(GPIOA, PIN5, 1, 0, 0, 0);			//输出， 接LED
+	GPIO_Init(GPIOD, PIN0, 1, 0, 0, 0);			//输出， 接LED
+	GPIO_Init(GPIOB, PIN8, 1, 0, 0, 0);
+	GPIO_Init(GPIOB, PIN9, 1, 0, 0, 0);
 	
 	while(1==1)
 	{
-		if(GPIO_GetBit(GPIOA, PIN4) == 0)		//按键被按下
+		if(GPIO_GetBit(GPIOB, PIN0) == 0)		//按键被按下
 		{
-			GPIO_SetBit(GPIOA, PIN5);
+			GPIO_SetBit(GPIOB, PIN8);
+			GPIO_ClrBit(GPIOB, PIN9);
+			GPIO_SetBit(GPIOD, PIN0);
 		}
 		else
 		{
-			GPIO_ClrBit(GPIOA, PIN5);
+			GPIO_ClrBit(GPIOB, PIN8);
+			GPIO_SetBit(GPIOB, PIN9);
+			GPIO_ClrBit(GPIOD, PIN0);
 		}
 	}
 }
